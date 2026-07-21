975164a1d02e1828f3aeb92550942e571ba94e3e
Elysia <3060702723@qq.com>
2026-07-21 15:27:30 +0800

    refactor(default_chatter): 合并 pass_and_wait 和 stop_conversation 到 control 动作模块

--- plugins/default_chatter/actions/__init__.py
+++ plugins/default_chatter/actions/__init__.py
@@ -2,9 +2,8 @@
 
 from __future__ import annotations
 
-from .pass_and_wait import PassAndWaitAction
+from .control import PassAndWaitAction, StopConversationAction
 from .send_text import SendTextAction
-from .stop_conversation import StopConversationAction
 
 __all__ = [
     "SendTextAction",
